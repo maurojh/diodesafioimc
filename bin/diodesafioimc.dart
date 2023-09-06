2c037776176ef863e4574d4f346200fbd62358ea
maurojh <maurojh@gmail.com>
2023-09-06 07:29:48 -0300

    Implementados lerConsole e lerConsoleDouble

--- bin/diodesafioimc.dart
+++ bin/diodesafioimc.dart
@@ -9,34 +9,13 @@ void main(List<String> arguments) {
   String nome = '';
   double peso = 0;
   double altura = 0;
-  var linha;
 
   print('Bem vindo à calculadora de IMC');
 
   nome = Utilitarios.lerConsole('Digite o nome');
+  peso = Utilitarios.lerConsoleDouble('Digite o peso: ');
+  altura = Utilitarios.lerConsoleDouble('Digite a altura: ');
   
-  do {
-    print('Digite o peso: ');
-    linha = stdin.readLineSync(encoding: utf8);
-    try {
-      linha = linha.replaceFirst(',', '.');
-      peso = double.parse(linha);
-    } catch (e) {
-      print('Peso inválido!');
-    }
-  } while (peso == 0);
-
-  do {
-    print('Digite a altura: ');
-    linha = stdin.readLineSync(encoding: utf8);
-    try {
-      linha = linha.replaceFirst(',', '.');
-      altura = double.parse(linha);
-    } catch (e) {
-      print('Altura inválida!');
-    }
-  } while (altura == 0);
-
   Pessoa pessoa = Pessoa(nome, peso, altura);
   pessoa.mostraIMC();
 }
--- lib/classes/utilitarios.dart
+++ lib/classes/utilitarios.dart
@@ -8,11 +8,26 @@ class Utilitarios {
     do {
       var linha = stdin.readLineSync(encoding: utf8);
       valor = linha ?? '';
-      if(valor.trim() == '') {
+      if (valor.trim() == '') {
         print('Leitura inválida!');
         print(mensagem);
       }
     } while (valor.trim() == '');
+    return valor;
+  }
+
+  static double lerConsoleDouble(String mensagem) {
+    double valor = 0;
+    do {
+      print(mensagem);
+      var linha = stdin.readLineSync(encoding: utf8) ?? '';
+      try {
+        linha = linha.replaceFirst(',', '.');
+        valor = double.parse(linha);
+      } catch (e) {
+        print('Valor inválido!');
+      }
+    } while (valor == 0);
     return valor;
   }
 }
